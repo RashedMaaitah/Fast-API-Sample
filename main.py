16d0394109c080f478b01f9e4adf9258a11939e1
Rashed Al Maaitah <bahran568@gmail.com>
2025-07-19 16:52:41 +0300

    add another test endpoint :)

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ async def say_hello(name: str):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.post("/items/{item_id}")
+async def create_item(item_id: int, item: str):
+    return {"item_id": item_id, "item": item}
