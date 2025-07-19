c8716a716302d31adabd5bfc543749c95661f084
Rashed Al Maaitah <bahran568@gmail.com>
2025-07-19 16:39:33 +0300

    add test endpoint :)

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
